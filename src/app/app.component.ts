f6b43757a9227b8116506e687853d05782c30180
Jason Meng <jason.meng@presagis.com>
2022-09-12 19:51:05 -0400

    add stlye.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({  // declaration
   selector: 'app-root',   // html tag embed the component
   templateUrl: './app.component.html',  // html file
   styleUrls: ['./app.component.css']  //can have more one
